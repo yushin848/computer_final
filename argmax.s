5fbfaf2956688e104d99cad2586d59acbf43a568
yushin848 <yusin848@hanyang.ac.kr>
2021-05-24 00:44:37 +0900

    Fisrst upload

--- argmax.s
+++ argmax.s
@@ -12,13 +12,21 @@
 #	a0 is the first index of the largest element
 # =================================================================
 argmax:
-
+    add s1, a0, x0
+    add s2, a1, x0
+    lw s3, 0(a0)
+    li t0,0
     # Prologue
 
 
 loop_start:
-
-
+    beq t0, s2, loop_end
+    slli s4 t0, 2
+    add t1, s1, s4
+    lw t2, 0(t1) 
+    blt s3, t2, loop_continue
+    addi t0, t0, 1 
+    j loop_start
 
 
 
@@ -26,6 +34,10 @@ loop_start:
 
 
 loop_continue:
+    add s3, t2, x0
+    add t3, t0, x0
+    add a0, t3, x0
+    j loop_start
 
 
 
--- relu.s
+++ relu.s
@@ -4,28 +4,31 @@
 # ==============================================================================
 # FUNCTION: Performs an inplace element-wise ReLU on an array of ints
 # Arguments:
-# 	a0 is the pointer to the array
-#	a1 is the # of elements in the array
+# 	a0 is the pointer to the array 
+#	a1 is the # of elements in the array 
 # Returns:
 #	None
 # ==============================================================================
 relu:
-    # Prologue
-
+   add t0 a0 x0 
+   add t1 a1 x0
+   li t2 0
 
 loop_start:
-    
-
-
-
-
-
-
+	slli t3 t2 2 
+	add t3 t3 t0 
+	lw t4 0(t3)
+	ble t4 x0 loop_continue
+	addi t2 t2 1 
+	beq t2 t1 loop_end
+	j loop_start
 
 loop_continue:
-
-
-
+	sw x0 0(t3)
+	addi t2 t2 1
+	beq t2 t1 loop_end
+	j loop_start
+	
 loop_end:
 
 
@@ -33,3 +36,4 @@ loop_end:
 
     
 	ret
+
